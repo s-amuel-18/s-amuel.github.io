a0ce70c102e1f8595fbb21cae017da6c6bc949bc
Samuel Graterol <63316336+s-amuel-18@users.noreply.github.com>
2026-03-12 09:51:30 -0400

    dsa

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://s-amuel-18.github.io',
+  site: 'https://s-amuel-18.github.io/portfolio',
   base: 'portfolio',
   i18n: {
     defaultLocale: 'en',
